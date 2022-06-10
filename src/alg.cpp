41a250cf583f1a83924fa73573736c6a793a309f
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 15:33:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 
 int cbinsearch(int *arr, int size, int value) {
-   int right = size - 1;
-   int left = 0, mid = 0;
-   while (left <= right) {
-   mid = (left + right) / 2;
-   if (value == *(arr + mid)) {
-   left = right = mid;
-   while (*(arr + right) == *(arr + right + 1)) {
-   right++;
-   }
-   while (*(arr + left) == *(arr + left - 1)) {
-   left--;
-   }
-   return right - left + 1;
-   } else if (value > *(arr + mid)) {
-   left = mid + 1;
-   } else if (value < *(arr + mid)) {
-   right = mid - 1;
-   }
-   }
-   return 0;
- }
+     int count = 0;
+     int s = size-1;
+     int v = value;
+     int s = size;
+     if (*arr == value) {
+         int i = 0;
+         while (arr[i] == value) {
+            count++;
+            i++;
+        }
+        return count;
+    }
+    if (arr[size-1] == value) {
+        int i = size-1;
+        while (arr[i] == value) {
+            count++;
+            i--;
+        }
+        return count;
+     }
+     int min = size;
+     int max = 0;
+     for (int i = 0; arr[min] != value && arr[max] != value && i != size / 2;i++) {
+     for (int i = 0; arr[min] != v && arr[max] != v && i != s / 2; i++) {
+         if (arr[(min+max)/2] > value) {
+             int i = max;
+             max = (min + max) / 2;
